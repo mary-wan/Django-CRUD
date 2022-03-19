e5e70a80f25ef874741590a3e43cc37851aeb499
mary-wan <mary.njenga@student.moringaschool.com>
2022-03-19 23:15:12 +0300

    create add employee functionality

--- employee/urls.py
+++ employee/urls.py
@@ -2,5 +2,6 @@ from django.urls.conf import path
 from . import views
 
 urlpatterns = [ 
-    
+    path('',views.add_employee, name='addEmployee'),
+
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from .forms import EmployeeForm
+from.models import Employee
+from django.urls import reverse
 
-# Create your views here.
+
+def add_employee(request):
+    employees = Employee.objects.all()
+    title ="ADD Employee"
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('addEmployee')
+
+    else:
+        form = EmployeeForm()
+
+
+
+    return render(request, 'addEmployee.html',{'employees':employees,'form':form,'title':title})
