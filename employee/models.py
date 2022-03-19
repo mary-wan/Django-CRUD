8afca83b1fddc526d44ad4fc736b8fccf5872808
mary-wan <mary.njenga@student.moringaschool.com>
2022-03-19 23:02:00 +0300

    create employee model

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=50, unique= True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
