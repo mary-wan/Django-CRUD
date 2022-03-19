a105eb9f0135df0cddd489a2ea1dcc750d4961a1
mary-wan <mary.njenga@student.moringaschool.com>
2022-03-19 23:37:11 +0300

    add delete employee function

--- employee/urls.py
+++ employee/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [ 
     path('',views.add_employee, name='addEmployee'),
     path('updateEmployee/<id>/',views.update_employee,name='updateEmployee'),
+        path('deleteStudent/<id>/',views.delete_employee,name='deleteEmployee')
 
 ]
--- employee/views.py
+++ employee/views.py
@@ -7,32 +7,33 @@ from django.urls import reverse
 
 def add_employee(request):
     employees = Employee.objects.all()
-    title ="ADD Employee"
+    title ="ADD EMPLOYEE"
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('addEmployee')
-
+            return HttpResponseRedirect(reverse("addEmployee"))
     else:
         form = EmployeeForm()
 
-
-
-    return render(request, 'addEmployee.html',{'employees':employees,'form':form,'title':title})
+    return render(request, 'add_employee.html',{'employees':employees,'form':form,'title':title})
 
 def update_employee(request,id):
     employees = Employee.objects.all()
-    title ="UPDATE STUDENT"
+    title ="UPDATE EMPLOYEE"
     employee = Employee.objects.get(id=id)
     if request.method == 'POST':
         form = EmployeeForm(request.POST,instance = employee)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse("home"))
+            return HttpResponseRedirect(reverse("addEmployee"))
 
     else:
         form = EmployeeForm(instance = employee)
 
-
     return render(request, 'update_employee.html',{'form':form,'employees':employees,'title':title})
+
+def delete_employee(request, id):  
+    employee = Employee.objects.get(id=id)  
+    employee.delete()  
+    return redirect("addEmployee")  
